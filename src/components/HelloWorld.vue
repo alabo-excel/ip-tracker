c01a24a3035e25899ded31bb341bb845338266fd
alabo-excel <iamalaboexcel@gmail.com>
2020-12-29 12:26:43 +0100

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,98 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <img src="../assets/pattern-bg.png" alt="" />
+    <div id="top">
+      <h3>Ip Address Tracker</h3>
+      <input
+        v-model="ip"
+        @blur="locationCheck"
+        type="text"
+        placeholder="Search for any Ip address or domain"
+      />
+    </div>
+    <div id="second" v-if="loca != null" class="d-flex justify-content-center p-5">
+      <div class="mx-5 text-left">
+        <p>IP ADDRESS</p>
+        <h6>{{ loca.ip }}</h6>
+      </div>
+      <div class="mx-5 text-left">
+        <p>LOCATION</p>
+        <h6>{{ loca.location.city }}, {{ loca.location.country }}</h6>
+        <h6>{{ loca.location.postalCode }}</h6>
+      </div>
+      <div class="mx-5 text-left">
+        <p>TIMEZONE</p>
+        <h6>{{ loca.location.timezone }}</h6>
+      </div>
+    </div>
+    <div v-else></div>
+    <div>
+      <GmapMap
+        :center="{ lat: 10, lng: 10 }"
+        :zoom="8"
+        map-type-id="hybrid"
+        style="width: 100%; height: 420px"
+      >
+      </GmapMap>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      map: null,
+      ip: "8.8.8.1",
+      loca: null,
+    };
+  },
+  mounted() {
+    // fetch("https://streetviewpublish.googleapis.com")
+    // .then((response) => response.json())
+    // .then((data) => console.log(data));
+  },
+  methods: {
+    locationCheck() {
+      fetch(
+        "https://geo.ipify.org/api/v1?apiKey=at_ppjjelGVj8AFNAlpiiNpkFUWi0w0G&ipAddress=" +
+          this.ip +
+          ""
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.loca = data;
+          console.log(this.loca);
+        });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+img {
+  width: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+input {
+  padding: 10px;
+  border: 0px;
+  border-radius: 10px;
+  width: 500px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#top {
+  position: absolute;
+  top: 60px;
+  left: 28%;
+  right: auto;
 }
-a {
-  color: #42b983;
+#mapid {
+  height: 180px;
+}
+#second{
+  position: absolute;
+  top: 10px;
+  background-color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps"
+
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: ''
+    },
+    installComponents: true
+})
+
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
